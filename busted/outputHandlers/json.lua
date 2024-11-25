6724e54151cd21c21943cb7994807219f6c8b64c
Yaro <yaro@mail.ru>
2024-11-25 20:20:01 +0000

    fix(outputHandler_json): for pending tests and json encoding

--- busted/outputHandlers/json.lua
+++ busted/outputHandlers/json.lua
@@ -1,10 +1,10 @@
-local json = require 'dkjson'
+local json = require("dkjson")
 local io_write = io.write
 local io_flush = io.flush
 
 return function(options)
-  local busted = require 'busted'
-  local handler = require 'busted.outputHandlers.base'()
+  local busted = require("busted")
+  local handler = require("busted.outputHandlers.base")()
 
   handler.suiteEnd = function()
     local error_info = {
@@ -12,14 +12,26 @@ return function(options)
       successes = handler.successes,
       failures = handler.failures,
       errors = handler.errors,
-      duration = handler.getDuration()
+      duration = handler.getDuration(),
     }
-    local ok, result = pcall(json.encode, error_info)
+
+    for _, test in ipairs(handler.pendings) do
+      test.element.attributes.default_fn = nil -- functions cannot be encoded into json
+    end
+
+    local ok, result = pcall(json.encode, error_info, {
+      exception = function(reason, value, state, default_reason)
+        local state_short = table.concat(state.buffer, "")
+        state_short = "..." .. state_short:sub(#state_short - 100) .. tostring(state.exception)
+        io.stderr:write(default_reason .. "(" .. state_short .. ")")
+      end,
+    })
 
     if ok then
       io_write(result)
     else
       io_write("Failed to encode test results to json: " .. result)
+      os.exit(1)
     end
 
     io_write("\n")
@@ -28,7 +40,7 @@ return function(options)
     return nil, true
   end
 
-  busted.subscribe({ 'suite', 'end' }, handler.suiteEnd)
+  busted.subscribe({ "suite", "end" }, handler.suiteEnd)
 
   return handler
 end
---
+++ spec/cl_output_json_spec.lua
@@ -0,0 +1,40 @@
+local utils = require("pl.utils")
+local path = require("pl.path")
+local busted_cmd = path.is_windows and "lua bin/busted" or "eval $(luarocks path) && bin/busted"
+local log = require('log')
+
+-- if exitcode >256, then take MSB as exit code
+local modexit = function(exitcode)
+  if exitcode > 255 then
+    return math.floor(exitcode / 256), exitcode - math.floor(exitcode / 256) * 256
+  else
+    return exitcode
+  end
+end
+
+local execute = function(cmd)
+  local success, exitcode, out, err = utils.executeex(cmd)
+  return not not success, modexit(exitcode), out, err
+end
+
+describe("Tests the busted json output", function()
+  it("encodes pending tests", function()
+    local success, exit_code, out, err = execute(busted_cmd .. ' ' .. "--pattern=cl_pending.lua$ --output=busted/outputHandlers/json.lua")
+
+    assert.is_true(success)
+    assert.is_true(exit_code == 0)
+    assert.is_true(err == '')
+  end)
+
+  it("notifies with error if results cannot be encoded", function()
+    local success, exit_code, out, err = execute(busted_cmd .. ' ' .. "--pattern=cl_output_json.lua$ --output=busted/outputHandlers/json.lua")
+
+    assert.is_false(success)
+    assert.is_true(exit_code ~= 0)
+    assert.is_truthy(err:find("type 'function' is not supported by JSON"))
+  end)
+
+  pending("encodes non-string resuls", function()
+    -- code
+  end)
+end)
